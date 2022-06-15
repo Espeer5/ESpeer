2a1fb5e5c25524a95f7196d2f38c7505d9080b1a
espeer <espeer@caltech.edu>
2022-06-14 22:37:34 -0500

    Add script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function drawMenu() {
+  var canvas = document.getElementById('menucan');
+  var ctx = canvas.getContext('2d');
+  ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
+  ctx.lineWidth = 1;
+  ctx.beginPath();
+  ctx.moveTo(0,1);
+  ctx.lineTo(20,1);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(0,10);
+  ctx.lineTo(20,10);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(0,19);
+  ctx.lineTo(20,19);
+  ctx.stroke();
+}
+
+drawMenu();
